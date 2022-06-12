d8dfb18a474ae95cf47ad6ba1d1183c39c12e478
Warxcell <warxcell@gmail.com>
2022-06-12 18:20:53 +0300

    add generics for writer.

--- src/WriterInterface.php
+++ src/WriterInterface.php
@@ -6,10 +6,19 @@ namespace Arxy\GraphQLCodegen;
 
 use Nette\PhpGenerator\PhpFile;
 
+/**
+ * @template T of ModuleInterface
+ */
 interface WriterInterface
 {
+    /**
+     * @param T $module
+     */
     public function init(ModuleInterface $module): void;
 
+    /**
+     * @param T $module
+     */
     public function write(ModuleInterface $module, PhpFile $file): void;
 }
 
