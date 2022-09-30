f7d1ccb627f282230770f4488c3916a683ca5712
Warxcell <warxcell@gmail.com>
2022-09-30 17:49:25 +0300

    Support enum extensions

---
+++ tests/Module1/Expected/Enum1.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Auto-Generated
+ */
+
+declare(strict_types=1);
+
+namespace Arxy\GraphQLCodegen\Tests\Module1\Expected;
+
+enum Enum1: string
+{
+    case VAL1 = 'VAL1';
+    case VAL2 = 'VAL2';
+    case VAL3 = 'VAL3';
+    case VAL4 = 'VAL4';
+}
