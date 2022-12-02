321ab573ad94112d2efa9ae615a395d9d8058ac4
Warxcell <warxcell@gmail.com>
2022-12-02 11:43:13 +0200

    add support for generics mapping

---
+++ tests/Module1/Expected/JSONResolverInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Auto-Generated
+ */
+
+declare(strict_types=1);
+
+namespace Arxy\GraphQLCodegen\Tests\Module1\Expected;
+
+interface JSONResolverInterface
+{
+    /**
+     * @param array<string, mixed> $value
+     */
+    public function serialize(array $value): string|null;
+
+    /**
+     * @return array<string, mixed>
+     * @throws \Exception
+     */
+    public function parseValue(string $value): array;
+
+    /**
+     * @param array<string, mixed>|null $variables
+     * @return array<string, mixed>
+     * @throws \Exception
+     */
+    public function parseLiteral(\GraphQL\Language\AST\Node $valueNode, ?array $variables = null): array;
+}
